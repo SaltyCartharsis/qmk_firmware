5d21f60dd7693b2f7895cf6ff1d1973a9eb69d6d
SaltyCartharsis <jmhunter77@gmail.com>
2023-06-21 00:58:59 -0400

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   keyboards/ploopyco/mouse/config.h
    	modified:   keyboards/ploopyco/mouse/info.json
    	modified:   keyboards/ploopyco/mouse/keymaps/default/keymap.c
    	modified:   keyboards/ploopyco/mouse/keymaps/drashna/keymap.c
    	modified:   keyboards/ploopyco/mouse/mouse.c
    	modified:   keyboards/ploopyco/mouse/mouse.h
    	modified:   keyboards/ploopyco/mouse/rules.mk
    	modified:   keyboards/ploopyco/trackball/config.h
    	modified:   keyboards/ploopyco/trackball/info.json
    	modified:   keyboards/ploopyco/trackball/keymaps/default/keymap.c
    	modified:   keyboards/ploopyco/trackball/readme.md
    	modified:   keyboards/ploopyco/trackball/rev1/config.h
    	new file:   keyboards/ploopyco/trackball/rev1/rev1.h
    	modified:   keyboards/ploopyco/trackball/rev1/rules.mk
    	modified:   keyboards/ploopyco/trackball/rev1_005/config.h
    	new file:   keyboards/ploopyco/trackball/rev1_005/rev1_005.h
    	modified:   keyboards/ploopyco/trackball/rev1_005/rules.mk
    	modified:   keyboards/ploopyco/trackball/rules.mk
    	modified:   keyboards/ploopyco/trackball/trackball.c
    	modified:   keyboards/ploopyco/trackball/trackball.h
    	modified:   keyboards/ploopyco/trackball_mini/config.h
    	modified:   keyboards/ploopyco/trackball_mini/info.json
    	modified:   keyboards/ploopyco/trackball_mini/keymaps/default/keymap.c
    	modified:   keyboards/ploopyco/trackball_mini/keymaps/drag_scroll/keymap.c
    	modified:   keyboards/ploopyco/trackball_mini/rev1_001/config.h
    	new file:   keyboards/ploopyco/trackball_mini/rev1_001/rev1_001.h
    	modified:   keyboards/ploopyco/trackball_mini/rev1_002/config.h
    	new file:   keyboards/ploopyco/trackball_mini/rev1_002/rev1_002.h
    	modified:   keyboards/ploopyco/trackball_mini/rules.mk
    	modified:   keyboards/ploopyco/trackball_mini/trackball_mini.h
    	modified:   keyboards/ploopyco/trackball_nano/config.h
    	modified:   keyboards/ploopyco/trackball_nano/info.json
    	modified:   keyboards/ploopyco/trackball_nano/keymaps/default/keymap.c
    	modified:   keyboards/ploopyco/trackball_nano/keymaps/lkbm/keymap.c
    	modified:   keyboards/ploopyco/trackball_nano/keymaps/maddie/keymap.c
    	modified:   keyboards/ploopyco/trackball_nano/rev1_001/config.h
    	new file:   keyboards/ploopyco/trackball_nano/rev1_001/rev1_001.h
    	modified:   keyboards/ploopyco/trackball_nano/rules.mk
    	modified:   keyboards/ploopyco/trackball_nano/trackball_nano.h
    	modified:   keyboards/ploopyco/trackball_thumb/config.h
    	modified:   keyboards/ploopyco/trackball_thumb/info.json
    	modified:   keyboards/ploopyco/trackball_thumb/keymaps/default/keymap.c
    	modified:   keyboards/ploopyco/trackball_thumb/rev1_001/info.json
    	new file:   keyboards/ploopyco/trackball_thumb/rev1_001/rev1_001.h
    	modified:   keyboards/ploopyco/trackball_thumb/trackball_thumb.c
    	modified:   keyboards/ploopyco/trackball_thumb/trackball_thumb.h

--- keyboards/ploopyco/trackball/rev1/rev1.h
+++ keyboards/ploopyco/trackball/rev1/rev1.h
@@ -15,27 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include QMK_KEYBOARD_H
 
-// safe range starts at `PLOOPY_SAFE_RANGE` instead.
+#pragma once
 
-// clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-                 C(KC_C), KC_BTN1, KC_BTN3, KC_BTN2, MO(1), KC_BTN4, KC_BTN5, DPI_CONFIG),
-    [1] = LAYOUT(
-                 RGB_HUI, RGB_MOD, RGB_TOG, RGB_RMOD, MO(1), KC_VOLU, KC_VOLD, QK_BOOT)
-};
-// clang-format on
-
-#ifdef RGBLIGHT_ENABLE
-void eeconkfig_init_user(void) {
-    rgblight_enable();
-#    ifdef RGBLIGHT_EFFECT_TWINKLE
-    rgblight_mode(RGBLIGHT_MODE_TWINKLE + 5);
-#    else
-    rgblight_mode(RGBLIGHT_MODE_BREATHING + 5);
-#    endif
-    rgblight_sethsv(HSV_MAGENTA);
-}
-#endif
+#include "trackball.h"
